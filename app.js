53470463781155cac4fc860f99c830ef25094dd7
darrenjon <darrenlin23@gmail.com>
2023-01-04 10:39:59 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 // require packages used in the project
 const express = require('express')
+const handlebars = require('express-handlebars')
 const methodOverride = require('method-override')
 
 const routes = require('./routes')
 
 const app = express()
 const port = process.env.PORT || 3000
+
+app.engine('hbs', handlebars({ extname: '.hbs' }))
+app.set('view engine', 'hbs')
 
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
